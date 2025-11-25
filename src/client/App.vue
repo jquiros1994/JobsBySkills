2707f44950fdd13d6d58ca52a57bfeafefe72457
jquiros1994 <quirosjulio1994@gmail.com>
2025-11-25 07:04:40 -0600

    Update App.vue

--- src/client/App.vue
+++ src/client/App.vue
@@ -1,44 +1,33 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div class="logo-container">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <span class="plus">+</span>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app-container">
+    <Navbar />
+    <main class="main-content">
+      <router-view />
+    </main>
   </div>
-  <HelloWorld msg="Servidor JobsBySkills activo." />
 </template>
 
+<script>
+import Navbar from './components/Navbar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+  },
+};
+</script>
+
 <style scoped>
-.logo-container {
+#app-container {
   display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 1.5em;
-  margin-bottom: 2rem;
+  flex-direction: column;
+  min-height: 100vh;
 }
 
-.plus {
-  font-size: 2.5em;
-  color: #cfcfcf;
-}
-
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.main-content {
+  flex: 1;
+  padding-top: 2rem;
+  padding-bottom: 2rem;
 }
 </style>
